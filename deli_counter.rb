5f633addcdd9a6c12fbf54fb2a1dd0c9d9be5744
Christopher Dutcher <cadutcher@icloud.com>
2020-06-22 17:33:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,6 +24,7 @@ def now_serving(katz_deli)
   if katz_deli.length >= 1
     next_in_line = katz_deli.first
     puts "Currently serving #{next_in_line}."
+    katz_deli.unshift
   else
     puts "There is nobody waiting to be served!"
   end
