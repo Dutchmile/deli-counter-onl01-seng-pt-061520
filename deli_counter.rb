1c4700526969fcfc0ff46249230599647dd64358
Christopher Dutcher <cadutcher@icloud.com>
2020-06-22 17:12:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,8 @@ puts "The line is currently:#{current_line.join}"
 else
   puts "The line is currently empty."
   end
+end
 
-def take_a_number(katz_deli, "new_customer")
+def take_a_number(katz_deli, new_customer)
 
 end
