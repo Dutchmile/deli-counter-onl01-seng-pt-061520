b728b0e5d4c4ec3cb400d07c12372f60d501c8e4
Christopher Dutcher <cadutcher@icloud.com>
2020-06-22 16:37:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,10 @@
-def line(katz_deli)
+katz_deli = []
+place_in_line = katz_deli.index
+
+def line(katz_deli, place_in_line)
+  if katz_deli.length >= 1
+    puts katz_deli.join("#{place_in_line}. ")
+  else
   puts "The line is currently empty."
+end
   end
