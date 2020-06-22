b4564dccc7defdbcd708dac313756b2389e72c9d
Christopher Dutcher <cadutcher@icloud.com>
2020-06-22 16:30:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,3 @@
 def line(katz_deli)
-  katz_deli.each do |number_in_line|
-    if number_in_line == 0
-      puts "The line is currently empty."
-    end
-  end
+  puts "The line is currently empty."
   end
